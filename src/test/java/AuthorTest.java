d1faa0adab76522a49b76f1abebac061890013d0
Illia Kuchko and Nathan Romike <nathanromike@gmail.com>
2016-01-27 08:18:33 -0800

    initiate project files.

---
+++ src/test/java/AuthorTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import java.util.List;
+
+public class AuthorTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  // @Test
+  // public void all_emptyAtFirst() {
+  //   assertEquals(Student.all().size(), 0);
+  // }
+
+
+}
---
+++ src/test/java/BookTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import java.util.List;
+
+public class BookTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  // @Test
+  // public void all_emptyAtFirst() {
+  //   assertEquals(Course.all().size(), 0);
+  // }
+
+
+}
